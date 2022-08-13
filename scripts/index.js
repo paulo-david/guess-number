0f0ee8191d323eae3ec97d9afeb64fdcf3a27038
paulo <paulodgsouza@gmail.com>
2022-08-13 16:28:06 -0300

    :sparkles: basic game logic

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,83 @@
+const API_URL =
+  "https://us-central1-ss-devops.cloudfunctions.net/rand?min=1&max=300";
+
 let drawNumber = 0;
 
-textResponse = document.getElementById("text-response");
-displayList = document.getElementById("display-list");
+let textResponse = document.getElementById("text-response");
+let displayList = document.getElementById("display-list");
+let guessForm = document.getElementById("guess-form");
+let guessInput = document.getElementById("guess-input");
+
+// update page to initial state, before game begins
+const cleanValues = () => {
+  textResponse.innerText = "";
+  printNumber(0);
+  // make the button of nova partida - desaparecer
+  guessInput.value = "";
+
+  // make the button orange
+};
 
 const startGame = () => {
-  printNumber(0);
+  cleanValues();
 
-  userGuess = document.getElementById("user-guess");
-  userGuess.innerText = "";
-  textResponse.innerText = "ASDF";
   // get the number from api
-  // if (error) {
-  //   text-response = "ERRO"
-  //   coloca o text-response e o display-list em red
-  //   aparece botão, nova partida
-  // }
+  fetch(API_URL)
+    .then((response) => response.json())
+    .then((response) => {
+      drawNumber = response.value;
+      console.log(`drawNumber is ${drawNumber}`);
+    })
+    .catch((err) => {
+      //   text-response =  "ERRO" fica RED
+      //   button nova partida, aparece
+      //  button enviar, fica gray
+    });
 };
 
 const printNumber = (number) => {
-  console.log(displayList);
   displayList.innerText = number;
 };
+
+guessForm.addEventListener("submit", (event) => {
+  // prevent page reload
+  event.preventDefault();
+
+  // check if the game is still going
+  if (drawNumber) {
+    userGuess = Number(guessInput.value);
+
+    // check if user guess is valid
+    if (userGuess) {
+      console.log(`userGuess is ${userGuess}`);
+
+      if (userGuess === drawNumber) {
+        textResponse.innerText = "Você acertou!!!";
+        // display turn green
+        // botton new-game appears
+        // guessInput, guessButton turns gray
+
+        drawNumber = 0;
+      } else if (userGuess > drawNumber) {
+        textResponse.innerText = "É menor";
+      } else {
+        textResponse.innerText = "É maior";
+      }
+
+      printNumber(userGuess)
+      
+    }
+  }
+});
+
+// if user guess is NOT valid, submit button turns gray
+guessInput.addEventListener("input", (event) => {
+  userGuess = guessInput.value;
+
+  // check if guess is NOT valid: guess NOT empty and guess is NOT a number
+  if (userGuess.length && !Number(userGuess)) {
+    console.log("turn the enviar button grayyy");
+  }
+});
 
 startGame();
